b204fc50eda337f60102ba87ab0bb81604546bf6
Matt Higgins <higginsmt@gmail.com>
2014-02-12 12:03:55 -0500

    bumped version to 0.0.3

--- lib/first_gem_matt_h44.rb
+++ lib/first_gem_matt_h44.rb
@@ -2,6 +2,6 @@ require "first_gem_matt_h44/version"
 
 class String
 	def word_count
-		self.split.word_countend
+		self.split.count
 	end
 end
--- lib/first_gem_matt_h44/version.rb
+++ lib/first_gem_matt_h44/version.rb
@@ -1,3 +1,3 @@
 module FirstGemMattH44
-  VERSION = "0.0.2"
+  VERSION = "0.0.3"
 end
